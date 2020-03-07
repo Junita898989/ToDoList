c02e82811e20f06748400445ba5e7babdfc471eb
KevinManimau <kevin03manimau@gmail.com>
2020-03-07 16:06:58 +0800

    Updates

---
+++ precache-manifest.081b719e47fed779d064d51a88a1a9b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90acde5dc67d370ce6f354b93e2ce4d",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "f2dc256029f95dda2dc8",
+    "url": "/ToDoList/static/css/main.cd34639d.chunk.css"
+  },
+  {
+    "revision": "b382b01ca048439b2f46",
+    "url": "/ToDoList/static/js/2.abbf2626.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ToDoList/static/js/2.abbf2626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2dc256029f95dda2dc8",
+    "url": "/ToDoList/static/js/main.3261053d.chunk.js"
+  },
+  {
+    "revision": "715a150e188fb2d1f286",
+    "url": "/ToDoList/static/js/runtime-main.f399f34c.js"
+  }
+]);
